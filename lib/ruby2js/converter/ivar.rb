68ec7d3f107e7a497c6438c22a8010579f3467de
Sam Ruby <rubys@intertwingly.net>
2013-12-15 14:15:50 -0500

    format replaced ivars

--- lib/ruby2js/converter/ivar.rb
+++ lib/ruby2js/converter/ivar.rb
@@ -5,7 +5,10 @@ module Ruby2JS
 
     handle :ivar do |var|
       if self.ivars and self.ivars.include? var
-        Ruby2JS.convert(self.ivars[var].inspect)
+        ruby2js = Ruby2JS::Converter.new(Ruby2JS.parse(self.ivars[var].inspect))
+        ruby2js.width = @width
+        ruby2js.enable_vertical_whitespace if @nl == "\n"
+        ruby2js.to_js
       else
         var.to_s.sub('@', 'this._')
       end
