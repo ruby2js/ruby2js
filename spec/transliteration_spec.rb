8237ea98ef10b532490058f499aecec4e704b930
Sam Ruby <rubys@intertwingly.net>
2014-05-27 00:38:56 -0400

    correct test

--- spec/transliteration_spec.rb
+++ spec/transliteration_spec.rb
@@ -609,12 +609,12 @@ describe Ruby2JS do
     end
     
     it "should insert var self = this when needed" do
-      to_js('class C; def m; list.each do; @ivar; @ivar; end; end; end').
-        must_equal 'function C() {}; C.prototype = {get m() {var self = this; list.each(function() {self._ivar; self._ivar})}}'
+      to_js('class C; def m; list.each do; @ivar; end; end; end').
+        must_equal 'function C() {}; C.prototype = {get m() {var self = this; return list.each(function() {self._ivar})}}'
       to_js('class C; def m(); list.each do; @ivar; @ivar; end; end; end').
         must_equal 'function C() {}; C.prototype.m = function() {var self = this; list.each(function() {self._ivar; self._ivar})}'
-      to_js('class C < S; def m; list.each do; @ivar; @ivar; end; end; end').
-        must_equal 'function C() {S.call(this)}; C.prototype = Object.create(S); C.prototype.constructor = C; Object.defineProperty(C.prototype, "m", {enumerable: true, configurable: true, get: function() {var self = this; list.each(function() {self._ivar; self._ivar})}})'
+      to_js('class C < S; def m; list.each do; @ivar; end; end; end').
+        must_equal 'function C() {S.call(this)}; C.prototype = Object.create(S); C.prototype.constructor = C; Object.defineProperty(C.prototype, "m", {enumerable: true, configurable: true, get: function() {var self = this; return list.each(function() {self._ivar})}})'
       to_js('class C < S; def m(); list.each do; @ivar; @ivar; end; end; end').
         must_equal 'function C() {S.call(this)}; C.prototype = Object.create(S); C.prototype.constructor = C; C.prototype.m = function() {var self = this; list.each(function() {self._ivar; self._ivar})}'
     end
