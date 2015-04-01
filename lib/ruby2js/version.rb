c97b5179b462e70bfa1556bfbcefb42038e19d2a
Sam Ruby <rubys@intertwingly.net>
2015-04-01 09:33:06 -0400

    push out version 2.0

--- lib/ruby2js/version.rb
+++ lib/ruby2js/version.rb
@@ -1,8 +1,8 @@
 module Ruby2JS
   module VERSION #:nodoc:
-    MAJOR = 1
-    MINOR = 15
-    TINY  = 1
+    MAJOR = 2
+    MINOR = 0
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
