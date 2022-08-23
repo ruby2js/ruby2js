93a04cf9859101a734152ecf889b1c6451c93668
Jared White <jared@jaredwhite.com>
2022-08-22 22:49:22 -0700

    Add delay to wait for esbuild test

--- packages/esbuild-plugin/test/test_esbuild.js
+++ packages/esbuild-plugin/test/test_esbuild.js
@@ -4,16 +4,21 @@ const fs = require('fs').promises
 describe('@ruby2js/esbuild-plugin', function() {
   this.timeout(5000);
 
-  it('runs code through ruby2js', async () => {
+  it('runs code through ruby2js', () => {
     require("./esbuild.config.js")
 
-    const code = await fs.readFile("app/assets/builds/application.js", { encoding: "utf-8"})
+    setTimeout(async () => {
+      const code = await fs.readFile(
+        "app/assets/builds/application.js",
+        { encoding: "utf-8"}
+      )
 
-    assert.strictEqual(
-`(() => {
-  // main.js.rb
-  console.log(parseInt("2A", 16));
-})();
-`, code)
+      assert.strictEqual(
+      `(() => {
+        // main.js.rb
+        console.log(parseInt("2A", 16));
+      })();
+      `, code)
+    }, 1000)
   })
 })
